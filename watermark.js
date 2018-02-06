bb5bccad3ae73b5eb649dce3ad7aa3e790385f59
hzgaomiaomiao <hzgaomiaomiao@corp.netease.com>
2018-02-06 11:06:47 +0800

    add: 居中显示

--- watermark.js
+++ watermark.js
@@ -1,35 +1,30 @@
 function init(option) {
-    let font, lineHeight;
-    font = 14;
+    option = Object.assign({
+        font: 14,
+        markTxt: '水印'
+    }, option);
 
-    let markTxt = option.markTxt;
-    let markTxtLength = option.markTxt.replace(/\u4e00-\u9fa5/gi, 'xx').length;
-    let markTxtWidth = markTxtLength * font;
+    let markTxtLength = option.markTxt.replace(/[\u4e00-\u9fa5]/gi, 'xx').length / 2;
+    let markTxtWidth = markTxtLength * option.font;
 
     let canvas = document.createElement('canvas');
-    let ctx = canvas.getContext('2d');
-    // lineHeight = 15; // this is guess and check as far as I know
-    ctx.font = `${font}px Arial`;
-
-    canvas.style.border = "1px solid";
     canvas.setAttribute('width', markTxtWidth * 2);
     canvas.setAttribute('height', markTxtWidth * 2);
 
-    ctx.translate(markTxtWidth * 1.5, markTxtWidth * 2 - font / 2);
-    ctx.rotate(-Math.PI / 4);
-    ctx.fillText(option.markTxt, 0, font);
+
+    let ctx = canvas.getContext('2d');
+    ctx.font = `${option.font}px Arial`;
+
+    ctx.translate(markTxtWidth, markTxtWidth); // translate to center of rotation
+    ctx.rotate(-Math.PI * 15 / 180);
+    ctx.translate(-markTxtWidth, -markTxtWidth); // translate back
+    ctx.fillText(option.markTxt, markTxtWidth / 2, markTxtWidth + option.font / 2);
 
     let dataURL = canvas.toDataURL();
-    appendToBody(dataURL);
-    // document.body.appendChild(canvas);
-}
-
-function appendToBody(dataURL) {
     let maskDiv = document.body.appendChild(document.createElement('div'));
     maskDiv.style.cssText = "position:fixed; width: 100%; height: 100%";
     maskDiv.style.backgroundImage = `url(${dataURL})`;
     maskDiv.style.backgroundRepeat = 'repeat';
-
 }
 
 const Watermark = {
